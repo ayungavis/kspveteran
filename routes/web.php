512a29c264be8649d17246b446acf7fc7597a2ab
Wahyu Adi Kurniawan <ayungavis@gmail.com>
2020-06-02 11:18:51 +0700

    Add routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +14,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'DashboardController@index')->name('dashboard')->middleware('auth');
+
+Route::resource('savings', 'SavingController')->middleware(['auth', 'super-admin']);
+Route::resource('installments', 'InstallmentController')->middleware(['auth', 'super-admin']);
+
+Route::get('customers', 'CustomerController@index')->name('customers.index')->middleware(['auth']);
+Route::get('customers/create', 'CustomerController@create')->name('customers.create')->middleware(['auth', 'super-admin']);
+Route::post('customers', 'CustomerController@store')->name('customers.store')->middleware(['auth', 'super-admin']);
+Route::get('customers/{customer}', 'CustomerController@show')->name('customers.show')->middleware(['auth']);
+Route::get('customers/{customer}/edit', 'CustomerController@edit')->name('customers.edit')->middleware(['auth', 'super-admin']);
+Route::put('customers/{customer}', 'CustomerController@update')->name('customers.update')->middleware(['auth', 'super-admin']);
+Route::delete('customers/{customer}', 'CustomerController@destroy')->name('customers.destroy')->middleware(['auth', 'super-admin']);
+
+Route::get('customers/{customer}/applications', 'CustomerController@application')->middleware('auth');
+Route::get('customers/{customer}/savings', 'CustomerController@saving')->middleware('auth');
+Route::get('customers/{customer}/installments', 'CustomerController@installment')->middleware('auth');
+
+Route::put('applications/{application}/approve', 'ApplicationController@approve')->middleware(['auth', 'head-office']);
+Route::put('applications/{application}/disapprove', 'ApplicationController@disapprove')->middleware(['auth', 'head-office']);
+
+Route::prefix('ajax')->group(function () {
+    Route::get('customers', 'AjaxController@customer')->middleware('auth');
+});
